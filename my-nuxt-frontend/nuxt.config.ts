03b1278745547a2cd17f3808f629495741219670
andrejs-poikans <andrejs.poikans@gmail.com>
2025-03-31 19:30:54 +0200

    Add static site for GitHub Pages

--- my-nuxt-frontend/nuxt.config.ts
+++ my-nuxt-frontend/nuxt.config.ts
@@ -2,7 +2,8 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
+  ssr: false,        // Disable SSR (Server-Side Rendering) for GitHub Pages
   css: [
     '@/css/style.css', // Path to the CSS file you just created
-  ]
+  ],
 })
