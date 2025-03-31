87db0f4ed81663c9b2ff4ca12c70603554ea5527
andrejs-poikans <andrejs.poikans@gmail.com>
2025-03-31 19:43:10 +0200

    re-try

--- my-nuxt-frontend/nuxt.config.ts
+++
@@ -1,9 +0,0 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true },
-  ssr: false,        // Disable SSR (Server-Side Rendering) for GitHub Pages
-  css: [
-    '@/css/style.css', // Path to the CSS file you just created
-  ],
-})
